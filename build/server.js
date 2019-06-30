1841732fb76341a35d4d5acbe4f94b667d3431f2
Carlos Marchena <sprintwithcarlos@pm.me>
2019-06-30 18:47:47 +0200

    Segregate Routes

--- build/server.js
+++ build/server.js
@@ -6,18 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 class Server {
     constructor() {
         this.app = express_1.default.application;
         this.app = express_1.default();
         this.config();
-        this.app.use(morgan_1.default("dev"));
-        this.app.use(helmet_1.default());
+        this.routes();
     }
     config() {
         this.app.set("port", process.env.PORT || 3000);
+        this.app.use(morgan_1.default("dev"));
+        this.app.use(helmet_1.default());
     }
-    routes() { }
+    routes() {
+        this.app.use(indexRoutes_1.default);
+    }
     start() {
         this.app.listen(this.app.get("port"), () => {
             console.log("Server listening on port", this.app.get("port"));
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,23 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 
+import indexRoutes from "./routes/indexRoutes";
+
 class Server {
   public app = express.application;
   constructor() {
     this.app = express();
     this.config();
-    this.app.use(morgan("dev"));
-    this.app.use(helmet());
+    this.routes();
   }
   config() {
     this.app.set("port", process.env.PORT || 3000);
+    this.app.use(morgan("dev"));
+    this.app.use(helmet());
   }
-  routes() {}
+  routes() {
+    this.app.use(indexRoutes);
+  }
 
   start() {
     this.app.listen(this.app.get("port"), () => {
