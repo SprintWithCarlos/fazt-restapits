6472659f6ef750246b5c702616326c51d41ee886
Carlos Marchena <sprintwithcarlos@pm.me>
2019-06-30 19:31:48 +0200

    Routes: First Part Completed

--- build/server.js
+++ build/server.js
@@ -6,7 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const compression_1 = __importDefault(require("compression"));
+const cors_1 = __importDefault(require("cors"));
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
+const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
 class Server {
     constructor() {
         this.app = express_1.default.application;
@@ -15,12 +19,23 @@ class Server {
         this.routes();
     }
     config() {
+        const MONGO_URI = "mongodb://localhost:27017/restapits";
+        mongoose_1.default
+            .connect(MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
+            .then(db => console.log("DB is connected"))
+            .catch(error => console.log(error));
+        //Settings
         this.app.set("port", process.env.PORT || 3000);
         this.app.use(morgan_1.default("dev"));
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
         this.app.use(helmet_1.default());
+        this.app.use(compression_1.default());
+        this.app.use(cors_1.default());
     }
     routes() {
         this.app.use(indexRoutes_1.default);
+        this.app.use("/api/posts", postRoutes_1.default);
     }
     start() {
         this.app.listen(this.app.get("port"), () => {
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import mongoose from "mongoose";
+import compression from "compression";
+import cors from "cors";
 
 import indexRoutes from "./routes/indexRoutes";
+import postRoutes from "./routes/postRoutes";
 
 class Server {
   public app = express.application;
@@ -12,12 +16,23 @@ class Server {
     this.routes();
   }
   config() {
+    const MONGO_URI = "mongodb://localhost:27017/restapits";
+    mongoose
+      .connect(MONGO_URI, { useNewUrlParser: true, useCreateIndex: true })
+      .then(db => console.log("DB is connected"))
+      .catch(error => console.log(error));
+    //Settings
     this.app.set("port", process.env.PORT || 3000);
     this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
     this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(cors());
   }
   routes() {
     this.app.use(indexRoutes);
+    this.app.use("/api/posts", postRoutes);
   }
 
   start() {
